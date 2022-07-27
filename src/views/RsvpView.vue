382e0fadf32f9440fdd8214a97452c88bec53f9b
Alexander Arvebratt <alexander.arvebratt@swegon.se>
2022-07-27 10:39:20 +0200

    Testing out forms.

---
+++ src/views/RsvpView.vue
@@ -0,0 +1,38 @@
+<template>
+  <body>
+    <form name="rsvp" method="POST" data-netlify="true">
+      <p>
+        <label>Personal code: <input type="text" name="code" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label
+          >Food preference:
+          <select name="role[]" multiple>
+            <option value="chicken">Chicken</option>
+            <option value="fish">Fish</option>
+            <option value="vegetarian">Vegetarian</option>
+          </select></label
+        >
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </body>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RsvpView",
+});
+</script>
+
+<style scoped></style>
